b2a5c70a5a7c2613f3304d8ae366728332ed7053
Tim Clark <tim.clark@outlook.com>
2017-04-04 21:22:57 -0400

    update chkpoint4-ng

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,22 +2,22 @@
      function config($stateProvider, $locationProvider) {
           $locationProvider
 			  .html5Mode({
-			  	enabled: true,
-			  	requireBase: false
+			  		enabled: true,
+			  		requireBase: false
          });
 		 
 		  $stateProvider
 			  .state('landing', {
-			  url: '/',
-			  templateUrl: '/templates/landing.html'
+			  		url: '/',
+			  		templateUrl: '/templates/landing.html'
          })
 			  .state('album', {
-			  url: '/album',
-			  templateUrl: '/templates/album.html'
+			  		url: '/album',
+			  		templateUrl: '/templates/album.html'
 		 })
               .state('collection', {
-              url: '/collection',
-              templateUrl: '/templates/collection.html'
+			  		url: '/collection',
+			  		templateUrl: '/templates/collection.html'
          });
 	 
 	 }
