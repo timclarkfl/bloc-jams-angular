cdbae03ba6ce9d4eef9a33e8a36aa6fd1ab3cc30
Tim Clark <tim.clark@outlook.com>
2017-04-17 16:31:56 -0400

    newchkpoint9-ng

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -79,7 +79,7 @@
                     playSong(song);
                 } else if (SongPlayer.currentSong === song) {
                     if (currentBuzzObject.isPaused()) {
-                        playSong(song);
+                        currentBuzzObject.play();
                     }
                 }
             };
